d041cfd5f6bf1109f36e1d985e507adc40c61f3d
Tanguy Marlot <marlot.tanguy@orange.fr>
2018-03-28 17:16:22 +0200

    Routes/Matchs GET OPEN

--- public/app.js
+++ public/app.js
@@ -1,48 +1,27 @@
-const blocPageDiv = document.getElementById("bloc_page")
+const mainDiv = document.getElementById('main')
 
 const render = html => {
-blocPageDiv.innerHTML = html
+  mainDiv.innerHTML = html
 }
 
-const match = {
-  equipeADomicile: "France",
-  equipeAExterieur: "Russie",
-  scoreExterieur: "4",
-  scoreDomicile: "6",
-  lieu: "",
-  heure: ""
-}
-document.getElementById("bloc_page").innerHTML = `${match.equipeADomicile}`;
+const controllers = {
 
-document.getElementById ("bloc_page").innerHTML = `<div class="card-deck">
-  <div class="card">
-    <img class="card-img-top" src="https://www.france.fr/data/svg-flags/fr.svg" alt="Card image cap">
-    <div class="card-body">
-      <h2 class="card-title text-center">${match.equipeADomicile} - ${match.equipeAExterieur}</h2>
-      <h3 class="card-text text-center">${match.scoreDomicile}-${match.scoreExterieur}</h3>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>`
+  '/': () => console.log("blabal")
+,
+  '*': () => render('<h1>Not Found</h1>')
+}
+
+const route = pathname => {
+  console.log("salut")
+}
+
+
+(() => {
+
+  ['/', '*'].forEach(
+    path => page(path, controllers[path])
+  )
+  page()
+  // route()
+
+})()
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const app = express()
-//const usersSeed = require('./public/pirates.json')
-let db
 
 app.use(express.static('public'))
 
 const html = `
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="fr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -15,24 +13,18 @@ const html = `
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
-    <div id="bloc_page">
+    <div id="main">
     </div>
     <script src="page.js"></script>
     <script src="app.js"></script>
   </body>
 </html>`
 
-/*
-app.post('/pirates', (req, res) => {
-  return insertUser(req.body)
-  .then(record => res.json(record))
+
+app.get('/matchs', (req, res) => {
+  console.log("matchs powa")
 })
 
-app.get('/pirates', (req, res) => {
-  db.all('SELECT * from users')
-  .then(records => res.json(records))
-})
-*/
 
 app.get('*', (req, res) => {
   res.send(html)
