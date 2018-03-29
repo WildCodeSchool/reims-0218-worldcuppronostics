a8442975138e882120916a668e3312efb50dbe87
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-03-29 15:00:11 +0200

    add form /matchs/new

--- public/app.js
+++ public/app.js
@@ -39,8 +39,32 @@ const controllers = {
     )
     
 ,
-  "/matchs/new": () => console.log('match/new')
+  "/matchs/new": () => render(
+    `<div class="container">
+      <div id="alert-box" class="hidden">
 
+      </div>
+      <form id="add-pirate">
+        <div class="form-group">
+          <label for="inputFirstName">First name</label>
+          <input name="firstName" type="text" class="form-control" id="inputFirstName" placeholder="Enter first name">
+        </div>
+        <div class="form-group">
+          <label for="inputLastName">Last name</label>
+          <input name="lastName" type="text" class="form-control" id="inputLastName" placeholder="Enter last name">
+        </div>
+        <div class="form-group">
+          <label for="inputImageUrl">Image URL</label>
+          <input name="image" type="text" class="form-control" id="inputImageUrl" placeholder="Enter image URL">
+        </div>
+        <div class="form-group">
+          <label for="inputBio">Bio</label>
+          <textarea name="bio" class="form-control" id="inputLastName" placeholder="Bio"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>`
+  )
 ,
   "*": () => render("<h1>Not Found</h1>")
   // toutes les autres routes sauf / on obtient en get NOT FOUND
