b6b3f2a1f467830ec286c3a427747cf77d815dde
Tanguy Marlot <marlot.tanguy@orange.fr>
2018-04-12 09:45:45 +0200

    tout est ok dans le fichier server.js, tous les matchs sont la et on peut s'inscrire

--- migrations/001-create-users-table.sql
+++ migrations/001-create-users-table.sql
@@ -11,3 +11,18 @@ CREATE TABLE matchs (
 
 -- Down
 DROP TABLE matchs;
+
+-- Up
+CREATE TABLE wilders (
+  id INTEGER PRIMARY KEY,
+  nom VARCHAR(50),
+  prenom VARCHAR(50),
+  pseudo VARCHAR(50),
+  mail VARCHAR(50),
+  motdepasse VARCHAR(50) ,
+  city VARCHAR(50),
+  equipepreferee VARCHAR(50)
+);
+
+-- Down
+DROP TABLE wilders;
--- server.js
+++ server.js
@@ -73,7 +73,7 @@ const html = `
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>Pronostics World Cup</title>
+      <title>Pronostics Wooorld Cup</title>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <body>
